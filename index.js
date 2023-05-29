006ff883539ea3f4ed06e59ee9a7427547c0f676
FN LN <51461857+dkolegaev@users.noreply.github.com>
2023-05-29 23:54:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-link = 'magnet:paste-magnet-link-here';
+link = 'magnet:?xt=urn:btih:C50F586A9BC9C1480450CDD06F6078E64A4AAF52&tr=http%3A%2F%2Fbt3.t-ru.org%2Fann%3Fmagnet&dn=%D0%A7%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA-%D0%BC%D1%83%D1%80%D0%B0%D0%B2%D0%B5%D0%B9%20%D0%B8%20%D0%9E%D1%81%D0%B0%3A%20%D0%9A%D0%B2%D0%B0%D0%BD%D1%82%D0%BE%D0%BC%D0%B0%D0%BD%D0%B8%D1%8F%20%2F%20Ant-Man%20and%20the%20Wasp%3A%20Quantumania%20(%D0%9F%D0%B5%D0%B9%D1%82%D0%BE%D0%BD%20%D0%A0%D0%B8%D0%B4%20%2F%20Peyton%20Reed)%20%5B2023%2C%20%D0%A1%D0%A8%D0%90%2C%20%D1%84%D0%B0%D0%BD%D1%82%D0%B0%D1%81%D1%82%D0%B8%D0%BA%D0%B0%2C%20%D0%B1%D0%BE%D0%B5%D0%B2%D0%B8%D0%BA%2C%20%D0%BA%D0%BE%D0%BC%D0%B5%D0%B4%D0%B8%D1%8F%2C%20%D0%BF%D1%80%D0%B8%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%BD%D0%B8%D1%8F';
 process.argv = ["cli", "download", link, "--keep-seeding"];
 require('./node_modules/webtorrent-cli/bin/cmd');
